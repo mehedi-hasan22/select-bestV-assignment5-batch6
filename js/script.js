ddc65bd4d83ec48192d8a1d47675a41a2ead12df
mehedi-hasan22 <mehedi30554@gmail.com>
2022-08-23 03:13:40 +0600

    made interactive cart

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-console.log('hello world')
+const cart = [];
+
+
+function addToCart(element) {
+    const playerName = element.parentNode.children[2].innerText;
+    cart.push(playerName);
+    console.log(cart)
+
+    let playerNumber = cart.length;
+    document.getElementById('selected').innerText = playerNumber;
+
+
+    const cartContainer = document.getElementById("player-cart");
+
+    const li = document.createElement("li");
+    li.innerText = `${playerName}`;
+    cartContainer.appendChild(li);
+}
+
